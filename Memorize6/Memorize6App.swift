e55dc5929dd25186a9ded564b3d871385a1e1b4a
Xiao Hu <xiao.hu@klm.com>
2022-08-31 15:00:13 +0200

    Initial Commit

---
+++ Memorize6/Memorize6App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize6App.swift
+//  Memorize6
+//
+//  Created by Xiao Hu on 31/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Memorize6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
